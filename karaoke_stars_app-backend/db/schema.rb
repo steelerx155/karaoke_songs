46edb43e252ab66222d74ad043840dd7b8f57f36
steelerx155 <steelerx155@gmail.com>
2020-05-29 13:11:35 -0400

    added genre model and added genre_id to songs

--- karaoke_stars_app-backend/db/schema.rb
+++ karaoke_stars_app-backend/db/schema.rb
@@ -10,15 +10,22 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_27_232022) do
+ActiveRecord::Schema.define(version: 2020_05_29_170952) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "genres", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "songs", force: :cascade do |t|
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "title"
+    t.integer "genre_id"
   end
 
 end
