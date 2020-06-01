574ec7e0203a2ec1649b829f466be41ffc42b003
steelerx155 <steelerx155@gmail.com>
2020-05-31 22:46:12 -0400

    added playlist model, controller and serializer

--- karaoke_stars_app-backend/db/schema.rb
+++ karaoke_stars_app-backend/db/schema.rb
@@ -10,13 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_29_193056) do
+ActiveRecord::Schema.define(version: 2020_06_01_023921) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "genres", force: :cascade do |t|
     t.string "name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "playlists", force: :cascade do |t|
+    t.string "songs"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
